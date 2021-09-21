4129b1bc7b17ef3fe4c7fa3511145bcfe1ca380a
TaneliM <64307246+TaneliM@users.noreply.github.com>
2021-09-21 11:18:30 +0300

    Comment ratings

--- src/index.js
+++ src/index.js
@@ -13,11 +13,17 @@ function initializeCode() {
   const removeCommentsButton = document.getElementById("remove-comments");
 
   addCommentButton.addEventListener("click", function () {
-    var commentsList = document.getElementById("comments-list");
-    var comment = document.createElement("li");
+    var commentsList = document.getElementById("comments");
+    var comment = document.createElement("div");
+    comment.className = "comment";
+
+    comment.appendChild(
+      document.createTextNode(document.getElementById("rating-select").value)
+    ).classNAme = "comment-rating";
+
     comment.appendChild(
       document.createTextNode(document.getElementById("new-comment").value)
-    );
+    ).className = "comment-text";
     commentsList.appendChild(comment);
   });
 
